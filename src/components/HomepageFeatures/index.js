fce18718d1a84fdd4e00fdef12d545f1c7c80e0d
Atlas Fyber <78761344+atlasfyber@users.noreply.github.com>
2024-07-17 14:15:42 +0300

    Update index.js

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -166,7 +166,7 @@ function NewsArticles() {
         </div>
           <div className={styles.loadingText}>
             <h4>🕝 Preparing some things for you..</h4>
-            <p>We're loading the content now. <br> </br> Depending on your connection and device, this may take a moment.</p>
+            <p>We're loading the news content, please wait. <br></br>Depending on your connection and device, this may take a moment.</p>
           </div>
         </div>
     );
